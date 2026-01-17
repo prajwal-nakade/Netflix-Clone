1d63bbefae78ab59280721f1ac88e7365d1e0a78
prajwalnakade <prajwalnakade478@gmail.com>
2026-01-17 13:24:11 +0530

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar.jsx'
+
+const Home = () => {
+  return (
+    <div className='home'><Navbar/></div>
+  )
+}
+
+export default Home
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Player.scss'
+
+const Player = () => {
+  return (
+    <div>Player</div>
+  )
+}
+
+export default Player
